39bc1acb3077125f4cb39b2fcad203b1354e61e0
Songkit Phachareon <severx002@gmail.com>
2024-07-29 09:20:59 +0700

    added missing images

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -1,9 +1,9 @@
-"use client";
-import React from "react";
+'use client'
+import React from 'react'
 
-import { motion } from "framer-motion";
-import { slideInFromTop } from "@/utils/motion";
-import Image from "next/image";
+import { motion } from 'framer-motion'
+import { slideInFromTop } from '@/utils/motion'
+import Image from 'next/image'
 
 const Encryption = () => {
   return (
@@ -15,8 +15,8 @@ const Encryption = () => {
         >
           Performance
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-            {" "}
-            &{" "}
+            {' '}
+            &{' '}
           </span>
           Security
         </motion.div>
@@ -62,7 +62,7 @@ const Encryption = () => {
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Encryption;
+export default Encryption
