63ed7de83fb7b621edcbb516d55ba68704c9a3fa
Songkit Phachareon <severx002@gmail.com>
2024-07-29 10:08:51 +0700

    update responsive styles

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -12,11 +12,11 @@ const Navbar = () => {
   } = process.env
 
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-5 md:px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-0 lg:px-[10px]">
         <a
           href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
+          className="h-auto w-auto flex-row items-center hidden md:flex"
         >
           <Image
             src="/NavLogo.png"
@@ -25,12 +25,10 @@ const Navbar = () => {
             height={70}
             className="cursor-pointer hover:animate-slowspin"
           />
-          <span className="font-bold hidden md:block text-gray-300">
-            {appName}
-          </span>
+          <span className="font-bold text-gray-300">{appName}</span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between">
+        <div className="w-[500px] h-full flex flex-row items-center justify-between ml-0 md:ml-8 lg:ml-0">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
